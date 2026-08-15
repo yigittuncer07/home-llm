38aefc4a72e4206cb4cd450100882be5fc99c210
YigitTuncer07 <yigittuncer22@gmail.com>
2026-08-15 17:48:37 +0300

    circular import bugfix

--- app/backend/models/message.py
+++ app/backend/models/message.py
@@ -1,8 +1,11 @@
+from typing import TYPE_CHECKING
 from datetime import datetime
 from sqlalchemy import String, Text, ForeignKey, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from models.chat import Chat
+if TYPE_CHECKING:
+    from models.chat import Chat
+
 from models.base import Base
 
 class Message(Base):
